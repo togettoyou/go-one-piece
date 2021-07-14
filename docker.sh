e5ce1a177f32a8be930bcfa3d561df38fe8ba736
togettoyou <zoujh99@qq.com>
2021-07-14 11:04:50 +0800

    gos v1.1.1

--- docker.sh
+++ docker.sh
@@ -4,4 +4,7 @@ echo 编译镜像...
 docker build -t gos/server:v1 .
 
 echo 运行...
-docker run --rm -ti -p 8888:8888 --mount type=bind,source=$PWD/conf,target=/root/togettoyou/conf gos/server:v1
+docker run --rm -ti -p 8888:8888 \
+  -v $PWD/conf:/root/togettoyou/conf \
+  -v $PWD/log:/root/togettoyou/log \
+  gos/server:v1
